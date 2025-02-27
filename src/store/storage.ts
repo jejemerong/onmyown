c082f3eb8af64ee2c9610568ddb9d88a9872297c
omomerong <omomerong@gmail.com>
2025-02-27 21:44:56 +0900

    feat: RN mmkv 스토리지 추가

--- src/store/storage.ts
+++ src/store/storage.ts
@@ -1,3 +1,13 @@
 import { MMKV } from 'react-native-mmkv'
+import { getAppGroupDirectory } from '../../NativeModules/RCTUserDefaultsModule'
 
-export const storage = new MMKV()
+// 사용 예시
+getAppGroupDirectory().then((path) => {
+  console.log('Storage Path:', path)
+})
+
+// TODO: path 변수로 수정, 각 디렉토리가 어떤 걸 의미하는지 변수로 치환할 수 있는지 테스트하기
+export const storage = new MMKV({
+  id: 'onmyown',
+  path: `/private/var/mobile/Containers/Shared/AppGroup/44FFCCC1-541E-4D7F-B40C-DA359C0A8C8F`,
+})
