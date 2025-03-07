278fe80e4f123f3c2d92d9c147c6ef36e7da4712
omomerong <omomerong@gmail.com>
2025-03-07 21:49:19 +0900

    feat: redux Provider 설정

--- index.js
+++ index.js
@@ -5,5 +5,18 @@
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
+import { persistStore } from 'redux-persist'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store } from './src/store/storage'
 
-AppRegistry.registerComponent(appName, () => App)
+AppRegistry.registerComponent(appName, () => () => {
+  const persistor = persistStore(store)
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  )
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { SafeAreaView, useColorScheme } from 'react-native'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { HomeScreen, LoginScreen, ProfileScreen } from './screens'
-import { reduxStorage } from './store/storage'
+import { LoginScreen, ProfileScreen } from './screens'
+import { useAppSelector } from './store/storage'
 
 // createClient
 const queryClient = new QueryClient()
@@ -14,36 +14,29 @@ const Stack = createNativeStackNavigator()
 const App = () => {
   const theme = useColorScheme()
   const isDarkTheme = theme === 'dark'
-  // TODO: useAppSelector 로 변경
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-  useEffect(() => {
-    reduxStorage.getItem('isLoggedIn').then((isLoggedIn: boolean) => {
-      console.log('isLoggedIn', isLoggedIn)
-      setIsLoggedIn(isLoggedIn)
-    })
-  }, [])
+  const isLoggedIn = useAppSelector((state) => state.root.user.isLoggedIn)
+  console.log('현재 로그인 상태: ', isLoggedIn)
 
   return (
     <SafeAreaView
       style={{ flex: 1, backgroundColor: isDarkTheme ? 'black' : 'white' }}
     >
-      <QueryClientProvider client={queryClient}>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName={isLoggedIn ? 'Profile' : 'Login'}>
-            <Stack.Screen
-              name="Profile"
-              options={{ headerShown: false }}
-              component={ProfileScreen}
-            />
-            <Stack.Screen
-              name="Login"
-              options={{ headerShown: false }}
-              component={LoginScreen}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </QueryClientProvider>
+      {/* TODO: RootNavigationContainer 로 변경 */}
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={isLoggedIn ? 'Profile' : 'Login'}>
+          <Stack.Screen
+            name="Profile"
+            options={{ headerShown: false }}
+            component={ProfileScreen}
+          />
+          <Stack.Screen
+            name="Login"
+            options={{ headerShown: false }}
+            component={LoginScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   )
 }
